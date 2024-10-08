5f99a6bd471547b4e9f663468c9ae9c1c556af08
lumay1 <lumayyingying@gmail.com>
2024-10-09 04:36:30 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/2/', // 设置基础路径为仓库名
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
   },
-  base: '',
 })
